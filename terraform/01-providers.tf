2ee91c13bd8c0f89ff4dda1ed41742cb57cc021a
Gabor Kalmar <gakalmar@gmail.com>
2024-06-18 09:28:07 +0200

    Add TF providers

---
+++ terraform/01-providers.tf
@@ -0,0 +1,9 @@
+provider "aws" {
+  region = "eu-west-2"
+}
+
+provider "kubernetes" {
+  host                   = aws_eks_cluster.eks.endpoint
+  cluster_ca_certificate = base64decode(aws_eks_cluster.eks.certificate_authority[0].data)
+  token                  = data.aws_eks_cluster_auth.eks.token
+}
